03068714988dc3ce5b37b03165b63c7c044cc7b5
alie <mushyafaali@gmail.com>
2024-02-04 07:51:50 +0700

    aply query (subquery) in modul customer

--- main/app/moduls/_customers/customers_serializer.py
+++ main/app/moduls/_customers/customers_serializer.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
 
 from . import customers_model
+from app.moduls._customers_orders import customers_orders_model
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    total_orders = serializers.SerializerMethodField()
 
     class Meta:
         model = customers_model.Customer
-        fields = ['id', 'customer_name', 'phone', 'email']
+        fields = ['id', 'customer_name', 'phone', 'email', 'password', 'total_orders']
+
+    def get_total_orders(self, obj):
+        return customers_orders_model.OrderContainer.objects.filter(customer=obj).count()
 
 
 class ActCustomerSerializer(serializers.ModelSerializer):
